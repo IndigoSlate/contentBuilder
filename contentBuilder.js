6db3515f25606a0c6e0ff810f8ce24041a5d55d0
U-CORP\Soren.Smith <Soren.Smith@HQ-SURFACE-113.corp.indigoslate.com>
2016-09-16 13:25:36 -0700

    updating structure

--- contentBuilder.js
+++ contentBuilder.js
@@ -2,41 +2,42 @@ document.addEventListener('DOMContentLoaded',function(){
 	window.cB = new ContentBuilder();
 	cB.set(cB.content);
 	content = null;
-});
 
-function ContentBuilder(){
-	var api = {
-		set:set,
-		content:content || {}
-	};
-	return api;
-	function set(setter){
-		for(key in setter){
-			cB.content[key] = setter[key];
-			setVal(key,setter[key]);
+	function ContentBuilder(){
+		var api = {
+			set:set,
+			content:content || {}
 		};
-	};
-	function setVal(key,val,exclude){
-		var selector = "[isbind="+key+"]";
-		var elements = document.querySelectorAll(selector);
-		if(elements.length){
-			for(i=0;i<elements.length;i++){
-				var el = elements[i];
-				if(el.nodeName==='INPUT'||el.nodeName==='TEXTAREA'){
-					el.value = val;
-					el.oninput = update;
-				}else{
-					el.innerHTML = val;
-				}
+		return api;
+		function set(setter){
+			for(key in setter){
+				cB.content[key] = setter[key];
+				setVal(key,setter[key]);
 			};
 		};
+		function setVal(key,val,exclude){
+			var selector = "[isbind="+key+"]";
+			var elements = document.querySelectorAll(selector);
+			if(elements.length){
+				for(i=0;i<elements.length;i++){
+					var el = elements[i];
+					if(el.nodeName==='INPUT'||el.nodeName==='TEXTAREA'){
+						el.value = val;
+						el.oninput = update;
+					}else{
+						el.innerHTML = val;
+					}
+				};
+			};
+		};
+		function update(e){
+			var updater = {},
+			pos = e.target.selectionStart;
+			updater[e.target.attributes.isbind.value] = e.target.value;
+			set(updater);
+			e.target.selectionStart = pos;
+			e.target.selectionEnd = pos;
+		}
 	};
-	function update(e){
-		var updater = {},
-		pos = e.target.selectionStart;
-		updater[e.target.attributes.isbind.value] = e.target.value;
-		set(updater);
-		e.target.selectionStart = pos;
-		e.target.selectionEnd = pos;
-	}
-};
+	
+});
